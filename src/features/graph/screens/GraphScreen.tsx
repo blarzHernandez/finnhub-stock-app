db410a58493bf2d49ffed573fa61a9f522961650
blarz <info.redpaz@gmail.com>
2025-09-25 23:25:58 -0600

    chore: adds more placeholder screen

---
+++ src/features/graph/screens/GraphScreen.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const GraphScreen = () => {
+  return (
+    <View style={{flex:1, padding:12}}>
+      <Text>Graph Screen</Text>
+    </View>
+  );
+};
